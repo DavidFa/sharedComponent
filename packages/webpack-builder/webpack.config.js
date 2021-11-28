259d290c9231fe2608509f4ebee3a4bbf907f7cd
David Fa <david@netcorpgps.com.au>
2021-11-28 11:59:05 +1100

    webpack install

---
+++ packages/webpack-builder/webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        static: './dist',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+          
+                }
+            }
+        ]
+    },
+};
